e0ff361d9dc46e5dc684e890c5e8410aa2e6b9e6
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-17 10:11:04 +0700

    Menambahkan event-event Camera untuk inisialisasi, switch kamera, flash, dan pengambilan gambar

--- lib/bloc/camera_event.dart
+++ lib/bloc/camera_event.dart
@@ -2,3 +2,11 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 
 sealed class CameraEvent {}
+
+final class InitializeCamera extends CameraEvent {}
+final class SwitchCamera extends CameraEvent {}
+final class ToggleFlash extends CameraEvent {}
+final class TakePicture extends CameraEvent {
+  final void Function(File imageFile) onPictureTaken;
+  TakePicture(this.onPictureTaken);
+}
