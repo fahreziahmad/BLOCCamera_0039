2f080bdda95b491f21242b6e6bf1bef539703ffa
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-17 17:49:17 +0700

    Menambahkan halaman CameraPage dan inisialisasi kamera menggunakan CameraBloc saat halaman dibuka

---
+++ lib/pages/camera_page.dart
@@ -0,0 +1,23 @@
+import 'package:camera/camera.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:kamera/bloc/camera_bloc.dart';
+import 'package:kamera/bloc/camera_event.dart';
+import 'package:kamera/bloc/camera_state.dart';
+
+class CameraPage extends StatefulWidget {
+  const CameraPage({super.key});
+
+  @override
+  State<CameraPage> createState() => _CameraPageState();
+}
+
+class _CameraPageState extends State<CameraPage> {
+  @override
+  void initState() {
+    super.initState();
+    final bloc = context.read<CameraBloc>();
+    if (bloc.state is! CameraReady) {
+      bloc.add(InitializeCamera());
+    }
+  }
