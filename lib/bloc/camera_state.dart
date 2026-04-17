32fcd68395120a5d02fbf8017b80e6d9c01291dd
fahreziahmad <alfahreziahmadsyahyan@gmail.com>
2026-04-17 10:14:42 +0700

    Menambahkan constructor pada state CameraReady untuk inisialisasi properti kamera

--- lib/bloc/camera_state.dart
+++ lib/bloc/camera_state.dart
@@ -11,3 +11,11 @@ final class CameraReady extends CameraState {
   final FlashMode flashMode;
   final File? imageFile;
   final String? snackbarMessage;
+
+  CameraReady({
+    required this.controller,
+    required this.selectedIndex,
+    required this.flashMode,
+    this.imageFile,
+    this.snackbarMessage,
+  });
